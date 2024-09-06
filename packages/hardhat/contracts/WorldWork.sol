0f4de9bf2b6eab167779e1071e7a837dfa8ba668
Andrzej <andrzej.daniel@archblock.com>
2024-09-06 20:37:31 +0200

    Add world id intergration

---
+++ packages/hardhat/contracts/WorldWork.sol
@@ -0,0 +1,186 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import { ByteHasher } from "./helpers/ByteHasher.sol";
+import { IWorldID } from "./interfaces/IWorldID.sol";
+
+contract WorldWork {
+
+	// struct VisitDetails {
+	// 	uint price;
+	// 	bool paid;
+	// 	address doctor;
+	// 	address patient;
+	// }
+
+	mapping(address => bool) public workers;
+	mapping(address => bool) public employers;
+
+	// mapping(string => VisitDetails) public visitdetails;
+
+	// mapping(address => mapping(address => bool)) public doctorsPermissions;
+
+	using ByteHasher for bytes;
+
+	///////////////////////////////////////////////////////////////////////////////
+	///                                  ERRORS                                ///
+	//////////////////////////////////////////////////////////////////////////////
+
+	/// @notice Thrown when attempting to reuse a nullifier
+	error InvalidNullifier();
+
+	/// @dev The World ID instance that will be used for verifying proofs
+	IWorldID internal immutable worldId;
+
+	/// @dev The contract's external nullifier hash
+	uint256 internal immutable externalNullifier;
+
+	/// @dev The World ID group ID (always 1)
+	uint256 internal immutable groupId = 1;
+
+	/// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
+	mapping(uint256 => bool) internal nullifierHashes;
+
+	event EmployerRegistered(address indexed doctor, string filesCid);
+	event WorkerRegistered(address indexed patient);
+	// event VisitFinalized(
+	// 	address indexed patient,
+	// 	address indexed doctor,
+	// 	string visitCid,
+	// 	uint price
+	// );
+	// event TransactionPaid(string visitCid);
+
+	/// @param _worldId The WorldID instance that will verify the proofs
+	/// @param _appId The World ID app ID
+	/// @param _actionId The World ID action ID
+	constructor(
+		IWorldID _worldId,
+		string memory _appId,
+		string memory _actionId
+	) {
+		worldId = _worldId;
+		externalNullifier = abi
+			.encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
+			.hashToField();
+	}
+
+	/// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
+	/// @param root The root of the Merkle tree (returned by the JS widget).
+	/// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
+	/// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
+	/// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
+	function registerWorker(
+		address signal,
+		uint256 root,
+		uint256 nullifierHash,
+		uint256[8] calldata proof
+	) public {
+		// First, we make sure this person hasn't done this before
+		if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
+		require(!workers[signal], "Worker already registered");
+		require(!employers[signal], "Employer already registered");
+
+		// We now verify the provided proof is valid and the user is verified by World ID
+		worldId.verifyProof(
+			root,
+			groupId,
+			abi.encodePacked(signal).hashToField(),
+			nullifierHash,
+			externalNullifier,
+			proof
+		);
+
+		// We now record the user has done this, so they can't do it again (proof of uniqueness)
+		nullifierHashes[nullifierHash] = true;
+
+		workers[signal] = true;
+
+		emit WorkerRegistered(signal);
+
+		// Finally, execute your logic here, for example issue a token, NFT, etc...
+		// Make sure to emit some kind of event afterwards!
+	}
+
+	// function registerPatient() public {
+	//     patients.push(msg.sender);
+	// }
+
+	/// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
+	/// @param root The root of the Merkle tree (returned by the JS widget).
+	/// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
+	/// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
+	/// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
+	function registerEmployer(
+		address signal,
+		uint256 root,
+		uint256 nullifierHash,
+		uint256[8] calldata proof,
+		string calldata filesCid
+	) public {
+		// First, we make sure this person hasn't done this before
+		if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
+		require(!workers[signal], "Worker already registered");
+		require(!employers[signal], "Employer already registered");
+		// We now verify the provided proof is valid and the user is verified by World ID
+		worldId.verifyProof(
+			root,
+			groupId,
+			abi.encodePacked(signal).hashToField(),
+			nullifierHash,
+			externalNullifier,
+			proof
+		);
+
+		// We now record the user has done this, so they can't do it again (proof of uniqueness)
+		nullifierHashes[nullifierHash] = true;
+
+		employers[signal] = true;
+
+		emit EmployerRegistered(signal, filesCid);
+	}
+
+	// function finalizeVisit(
+	// 	address patient,
+	// 	address doctor,
+	// 	string calldata visitCid,
+	// 	uint price
+	// ) public {
+	// 	require(doctors[doctor], "Only doctors can add documents");
+	// 	require(patients[patient], "Only patients can have documents");
+    //     require(doctorsPermissions[doctor][patient], "Doctor does not have permission to access patient's profile");
+	// 	visitdetails[visitCid] = VisitDetails(price, false, doctor, patient);
+	// 	emit VisitFinalized(patient, doctor, visitCid, price);
+	// }
+
+	// function shareProfile(address doctor) public {
+	// 	require(doctors[doctor], "Only doctors can be shared with");
+	// 	require(patients[msg.sender], "Only patients can share their profile");
+	// 	doctorsPermissions[doctor][msg.sender] = true;
+	// }
+
+	// function revokeProfile(address doctor) public {
+	// 	require(doctors[doctor], "Only doctors can be shared with");
+	// 	require(patients[msg.sender], "Only patients can share their profile");
+	// 	doctorsPermissions[doctor][msg.sender] = false;
+	// }
+
+	// function payForVisit(string calldata visitCid) public payable {
+	// 	require(patients[msg.sender], "Only patients can pay for visits");
+	// 	require(
+	// 		visitdetails[visitCid].patient == msg.sender,
+	// 		"Only the patient can pay for the visit"
+	// 	);
+	// 	require(
+	// 		visitdetails[visitCid].paid == false,
+	// 		"The visit has already been paid for"
+	// 	);
+	// 	require(visitdetails[visitCid].price == msg.value, "Bad value");
+	// 	// Transfer the payment to the doctor
+	// 	payable(visitdetails[visitCid].doctor).transfer(msg.value);
+	// 	// Mark the visit as paid
+	// 	visitdetails[visitCid].paid = true;
+	// 	revokeProfile(visitdetails[visitCid].doctor);
+	// 	emit TransactionPaid(visitCid);
+	// }
+}
---
+++ packages/nextjs/app/worker/page.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useConnectModal } from '@rainbow-me/rainbowkit'
+import { IDKitWidget, ISuccessResult } from '@worldcoin/idkit'
+import { NextPage } from 'next'
+import { decodeAbiParameters } from 'viem'
+import { useAccount } from 'wagmi'
+import { worldAction, worldIdApp } from '~~/app/constants'
+import { Heading1 } from '~~/components/ui/Heading1'
+import { Heading3 } from '~~/components/ui/Heading3'
+import { useScaffoldWriteContract } from '~~/hooks/scaffold-eth'
+
+const PatientSignin: NextPage = () => {
+  const { openConnectModal } = useConnectModal()
+  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract('WorldWork')
+
+  const { isConnected, address } = useAccount()
+  const router = useRouter()
+
+  const onWalletConnect = () => {
+    if (isConnected) {
+      return
+    }
+    openConnectModal?.()
+  }
+
+  const onSuccess = async (result: ISuccessResult) => {
+    const unpackedProof = decodeAbiParameters([{ type: 'uint256[8]' }], result.proof as `0x${string}`)[0]
+    await writeYourContractAsync(
+      {
+        functionName: 'registerWorker',
+        args: [address, BigInt(result.merkle_root), BigInt(result.nullifier_hash), unpackedProof],
+      },
+      {
+        onSuccess: () => {
+          router.push('/register/patient/info')
+        },
+      },
+    )
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-10 mt-24">
+      <div className="flex flex-col gap-1">
+        <Heading1>Create account</Heading1>
+        <Heading3 className="mt-4">Connect with World ID to create account</Heading3>
+      </div>
+
+      {!address && (
+        <button onClick={onWalletConnect} className="btn btn-outline rounded-full">
+          <span>Connect Wallet</span>
+        </button>
+      )}
+
+      {address && (
+        <IDKitWidget
+          app_id={worldIdApp} // obtained from the Developer Portal
+          action={worldAction} // obtained from the Developer Portal
+          onSuccess={onSuccess} // callback when the modal is closed
+          signal={address} // the signal to be verified
+          // handleVerify={handleVerify} // callback when the proof is received
+          // verification_level={VerificationLevel.Orb}
+        >
+          {({ open }: { open: () => void }) => (
+            // This is the button that will open the IDKit modal
+            <button onClick={open} className="btn btn-outline rounded-full">
+             icon
+              <span>Connect with World ID</span>
+            </button>
+          )}
+        </IDKitWidget>
+      )}
+
+      <span className="text-xs">
+        Don&apos;t have World ID?{' '}
+        <u>
+          <b>Learn more</b>
+        </u>
+      </span>
+    </div>
+  )
+}
+
+export default PatientSignin

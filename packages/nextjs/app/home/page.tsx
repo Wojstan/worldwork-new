af8a00133f59ac65888352e7d943617c7dd47a6d
Wojstan <wpstaniszewski@gmail.com>
2024-09-06 19:57:13 +0200

    Allow connecting wallet

--- packages/nextjs/app/home/page.tsx
+++ packages/nextjs/app/home/page.tsx
@@ -1,19 +1,47 @@
 'use client'
 
 import Link from 'next/link'
+import { useConnectModal } from '@rainbow-me/rainbowkit'
 import { NextPage } from 'next'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import { Heading1 } from '~~/components/ui/Heading1'
 import { Heading3 } from '~~/components/ui/Heading3'
+import { useEffect, useState } from 'react'
+import { useAccount } from 'wagmi'
+import { useRouter } from "next/navigation";
 
 const Home: NextPage = () => {
+  const { openConnectModal } = useConnectModal()
+  const { isConnected } = useAccount()
+  const router = useRouter()
+
+  const [clickedConnect, setClickedConnect] = useState(false)
+
+  useEffect(() => {
+    if (clickedConnect && isConnected) {
+      router.push('/jobs')
+    }
+  }, [clickedConnect, isConnected, router]);
+
+  const onLogin = () => {
+    if (isConnected) {
+      router.push('/jobs')
+      return
+    }
+    openConnectModal?.()
+    setClickedConnect(true)
+  }
+
   return (
     <div className="flex flex-col justify-center items-center gap-8">
       <div className="flex flex-col justify-center items-center gap-4">
         <Heading1>Hello!</Heading1>
         <Heading3>Welcome to Worldwork platform</Heading3>
       </div>
-      <div className="card card-bordered border-black min-w-[900px] cursor-pointer hover:bg-[#CFAAFF]">
+      <div
+        className="card card-bordered border-black min-w-[900px] cursor-pointer hover:bg-[#CFAAFF]"
+        onClick={onLogin}
+      >
         <div className="card-body px-20 py-5">
           <div className="flex flex-row items-center gap-4 min-h-44 text-accent font-semibold">
             <span className="text-3xl">I already have an account</span>
--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -1,32 +1,29 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
-import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useTheme } from "next-themes";
-import { Toaster } from "react-hot-toast";
-import { WagmiProvider } from "wagmi";
-import { Footer } from "~~/components/Footer";
-import { Header } from "~~/components/Header";
-import { BlockieAvatar } from "~~/components/scaffold-eth";
-import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
-import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
-import { wagmiConfig } from "~~/services/web3/wagmiConfig";
+import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+import { WagmiProvider } from 'wagmi'
+import { Header } from '~~/components/Header'
+import { BlockieAvatar } from '~~/components/scaffold-eth'
+import { ProgressBar } from '~~/components/scaffold-eth/ProgressBar'
+import { useInitializeNativeCurrencyPrice } from '~~/hooks/scaffold-eth'
+import { wagmiConfig } from '~~/services/web3/wagmiConfig'
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
-  useInitializeNativeCurrencyPrice();
+  useInitializeNativeCurrencyPrice()
 
   return (
     <>
       <div className="flex flex-col min-h-screen bg-neutral-content">
-        {/* <Header /> */}
+        <Header />
         <main className="relative flex flex-col flex-1">{children}</main>
         {/* <Footer /> */}
       </div>
       <Toaster />
     </>
-  );
-};
+  )
+}
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -34,28 +31,17 @@ export const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
-});
+})
 
 export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
-  const { resolvedTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <ProgressBar />
-        <RainbowKitProvider
-          avatar={BlockieAvatar}
-          theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
-        >
+        <RainbowKitProvider avatar={BlockieAvatar} theme={lightTheme()}>
           <ScaffoldEthApp>{children}</ScaffoldEthApp>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
-  );
-};
+  )
+}

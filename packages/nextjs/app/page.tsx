1f7abd2916a39c5a3ee170cf3b57cbaa4b138b08
Wojstan <51679670+Wojstan@users.noreply.github.com>
2024-09-06 19:57:58 +0200

    🌈 Allow connecting wallet (#4)
    Allow connecting wallet

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import Link from 'next/link'
+import type { NextPage } from 'next'
+import { useAccount } from 'wagmi'
+import { BugAntIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { Address } from '~~/components/scaffold-eth'
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  const { address: connectedAddress } = useAccount()
 
   return (
     <>
@@ -22,17 +22,17 @@ const Home: NextPage = () => {
             <Address address={connectedAddress} />
           </div>
           <p className="text-center text-lg">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               packages/nextjs/app/page.tsx
             </code>
           </p>
           <p className="text-center text-lg">
-            Edit your smart contract{" "}
+            Edit your smart contract{' '}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               YourContract.sol
-            </code>{" "}
-            in{" "}
+            </code>{' '}
+            in{' '}
             <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
               packages/hardhat/contracts
             </code>
@@ -44,20 +44,20 @@ const Home: NextPage = () => {
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Tinker with your smart contract using the{' '}
                 <Link href="/debug" passHref className="link">
                   Debug Contracts
-                </Link>{" "}
+                </Link>{' '}
                 tab.
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
+                Explore your local transactions with the{' '}
                 <Link href="/blockexplorer" passHref className="link">
                   Block Explorer
-                </Link>{" "}
+                </Link>{' '}
                 tab.
               </p>
             </div>
@@ -65,7 +65,7 @@ const Home: NextPage = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home

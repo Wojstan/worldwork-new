fe185d60fdb2c52a3a3144b28a039c21b77f2b99
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-09-06 21:41:46 +0200

    📦 Add adding job flow (#10)
    * Add initial add job page
    
    * Add token icons
    
    * Add removeLabel prop to input
    
    * Add currency boxes

--- packages/nextjs/components/ui/Input.tsx
+++ packages/nextjs/components/ui/Input.tsx
@@ -5,17 +5,30 @@ interface InputProps {
   className?: string
   inputClass?: string
   textarea?: boolean
+  removeLabel?: boolean
 }
 
-export const Input = ({ id, label, placeholder, className, textarea }: InputProps) => {
+export const Input = ({ id, label, placeholder, className, textarea, removeLabel = false }: InputProps) => {
   return (
     <label className={className}>
-      <div className="label">{label && <span className="label-text">{label}</span>}</div>
+      {!removeLabel && <div className="label">{label && <span className="label-text">{label}</span>}</div>}
 
       {textarea ? (
-        <textarea id={id} name={id} rows={6} placeholder={placeholder} className="input p-3 input-bordered w-full bg-white h-auto mb-4" />
+        <textarea
+          id={id}
+          name={id}
+          rows={6}
+          placeholder={placeholder}
+          className="input p-3 input-bordered w-full bg-white h-auto mb-4"
+        />
       ) : (
-        <input id={id} name={id} type="text" placeholder={placeholder} className="input input-bordered w-full bg-white" />
+        <input
+          id={id}
+          name={id}
+          type="text"
+          placeholder={placeholder}
+          className="input input-bordered w-full bg-white"
+        />
       )}
     </label>
   )

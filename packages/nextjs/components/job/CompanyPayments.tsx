570b41df288dd0b16d30ef72bf67a236e9f508b0
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-09-07 21:24:55 +0200

    Add company payments tab

--- packages/nextjs/components/job/CompanyPayments.tsx
+++ packages/nextjs/components/job/CompanyPayments.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image'
 import { Heading1 } from '../ui/Heading1'
-import { useQuery } from "@tanstack/react-query";
-import { useAccount } from "wagmi";
-import { RequestNetwork, Types } from "@requestnetwork/request-client.js";
+import { RequestNetwork, Types } from '@requestnetwork/request-client.js'
+import { useQuery } from '@tanstack/react-query'
+import { formatUnits } from 'viem'
+import { useAccount, useEnsName } from 'wagmi'
+import { Salary } from '~~/components/Salary'
+import { DateInfo } from '~~/components/ui/DateInfo'
+import { Heading3 } from '~~/components/ui/Heading3'
+import { Loader } from '~~/components/ui/Loader'
+import { formatTime } from '~~/utils/formatTime'
+import { shortenText } from '~~/utils/shortenText'
 
 export function CompanyPayments() {
   const { address } = useAccount()
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['payments', address],
     queryFn: async () => {
       if (!address) {
@@ -13,20 +21,52 @@ export function CompanyPayments() {
       }
       const requestClient = new RequestNetwork({
         nodeConnectionConfig: {
-          baseURL: "https://gnosis.gateway.request.network",
+          baseURL: 'https://gnosis.gateway.request.network',
         },
-      });
+      })
       const requests = await requestClient.fromIdentity({
         type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
         value: address,
-      });
-      return requests.map((request) => request.getData());
-    }
+      })
+      return requests.map((request) => request.getData())
+    },
   })
+
+  if (isLoading) {
+    return <Loader />
+  }
 
   return (
     <div>
       <Heading1>Payments</Heading1>
+      <div className="flex flex-col gap-3 mt-8">
+        {data?.map((payment) => (
+          <PaymentBox key={payment.requestId} payment={payment} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+interface PaymentBoxProps {
+  payment: Types.IRequestDataWithEvents
+}
+
+const PaymentBox = ({ payment }: PaymentBoxProps) => {
+  const receiverAddress = payment.payee?.value
+  const { data } = useEnsName({ address: receiverAddress })
+  const amount = formatUnits(BigInt(payment.balance?.balance ?? 0), 18)
+  const date = new Date(payment.timestamp * 1000)
+
+  return (
+    <div className="border border-black h-28 rounded-xl bg-white z-50 flex items-center justify-between px-6 relative">
+      <div className="flex gap-8 items-center">
+        <Image alt="avatar" src="/doe.png" width={80} height={80} />
+        <div className="font-semibold text-lg">{data ?? shortenText(receiverAddress)}</div>
+      </div>
+      <Salary primary={amount} />
+      <DateInfo date={date} />
+      <span className="text-base text-[#356B00]">✓ PAID</span>
     </div>
   )
 }
---
+++ packages/nextjs/utils/formatTime.ts
@@ -0,0 +1,11 @@
+export function formatTime(date: Date) {
+  return new Intl.DateTimeFormat('default', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZoneName: 'short',
+    hour12: false,
+  }).format(date)
+}

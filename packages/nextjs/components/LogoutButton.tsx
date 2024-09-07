a6861cb9763c73971f2f38ee0bbc4443f931ffd4
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-09-07 17:15:30 +0200

    Read ens name in logout button

--- packages/nextjs/components/LogoutButton.tsx
+++ packages/nextjs/components/LogoutButton.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useAccount, useDisconnect } from 'wagmi'
+import { useAccount, useDisconnect, useEnsName } from 'wagmi'
+import { sepolia } from "viem/chains";
 
 export function LogoutButton() {
   const { address } = useAccount()
+  const { data } = useEnsName({ address, chainId: sepolia.id })
 
   const router = useRouter()
   const { disconnectAsync } = useDisconnect({
@@ -18,7 +20,7 @@ export function LogoutButton() {
 
   return (
     <div className="flex flex-row justify-center items-center bg-[#B881FF33] rounded-full font-semibold text-sm text-[#B881FF] pl-5">
-      {address}
+      {data || address}
       <button className="btn btn-outline rounded-full bg-white ml-3" onClick={() => disconnectAsync()}>
         Logout
       </button>

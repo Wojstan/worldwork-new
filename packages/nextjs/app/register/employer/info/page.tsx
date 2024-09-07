8aabeb029e178b56ffa4969b2cb15ede01e3a54e
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-09-07 02:09:21 +0200

    🛜 Add email to employer info (#16)
    Add email to employer info

--- packages/nextjs/app/register/employer/info/page.tsx
+++ packages/nextjs/app/register/employer/info/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { NextPage } from 'next'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { NextPage } from 'next'
 import { decodeAbiParameters } from 'viem'
 import { Button } from '~~/components/ui/Button'
 import { Heading1 } from '~~/components/ui/Heading1'
@@ -15,7 +15,7 @@ const EmployerInfo: NextPage = () => {
   const merkle_root = searchParams.get('merkle_root')
   const nullifier_hash = searchParams.get('nullifier_hash')
   const proof = searchParams.get('proof')
-  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("WorldWork");
+  const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract('WorldWork')
   const router = useRouter()
 
   const onSubmit = async (formData: FormData) => {
@@ -24,31 +24,29 @@ const EmployerInfo: NextPage = () => {
     }
 
     const unpackedProof = decodeAbiParameters([{ type: 'uint256[8]' }], proof as `0x${string}`)[0]
-    await writeYourContractAsync({
-      functionName: "registerEmployer",
-      args: [
-        address,
-        BigInt(merkle_root),
-        BigInt(nullifier_hash),
-        unpackedProof
-      ],
-    }, {
-      onSuccess: () => {
-        router.push('/company/offers')
-      }
-    })
-
+    await writeYourContractAsync(
+      {
+        functionName: 'registerEmployer',
+        args: [address, BigInt(merkle_root), BigInt(nullifier_hash), unpackedProof],
+      },
+      {
+        onSuccess: () => {
+          router.push('/company/offers')
+        },
+      },
+    )
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-10 mt-24">
-      <div className="flex flex-col gap-1 items-center">
-        <Heading1>Provide some info</Heading1>
-      </div>
-      <form className="form-control gap-6" action={onSubmit}>
+    <div className="flex flex-col items-center justify-center gap-10 mt-24 ">
+      <Heading1>Provide some info</Heading1>
+      <form className="form-control gap-4" action={onSubmit}>
         <Input id="name" label="Company Name" placeholder="DevCats" />
+        <Input id="email" label="E-mail" placeholder="john@devcats.com" />
         <Input id="nickname" label="Nickname" placeholder="devcats" className="w-full" />
-        <Button type="submit">Continue</Button>
+        <Button type="submit" className="mt-6">
+          Continue
+        </Button>
       </form>
     </div>
   )

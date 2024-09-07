f6072294eee82e382d1669bce4f3caa8480ae988
Wojstan <wpstaniszewski@gmail.com>
2024-09-07 22:32:12 +0200

    WIP

--- packages/nextjs/db/jobActions.ts
+++ packages/nextjs/db/jobActions.ts
@@ -1,18 +1,24 @@
-"use server"
+'use server'
 
-import { employee, job, NewJob } from "~~/db/schema";
-import { db } from "~~/db/drizzle";
-import { eq, and } from 'drizzle-orm'
-
+import { and, eq } from 'drizzle-orm'
+import { db } from '~~/db/drizzle'
+import { NewJob, employee, job } from '~~/db/schema'
 
 export const addJobOffer = (newJob: NewJob) => {
   return db.insert(job).values(newJob).returning()
 }
 
 export const getJobOffers = () => {
-  return db.select().from(job).leftJoin(employee, eq(job.employer, employee.wallet))
+  return db
+    .select()
+    .from(job)
+    .leftJoin(employee, eq(job.employer, employee.wallet))
 }
 
 export const getJob = (employer: string, index: number) => {
-  return db.select().from(job).where(and(eq(job.employer, employer), eq(job.arrayIndex, index))).leftJoin(employee, eq(job.employer, employee.wallet))
+  return db
+    .select()
+    .from(job)
+    .where(and(eq(job.employer, employer), eq(job.arrayIndex, index)))
+    .leftJoin(employee, eq(job.employer, employee.wallet))
 }

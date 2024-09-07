34ab6b9d83d4fc04a2f2de128917e971ef3c2c8f
Andrzej <andrzej.daniel@archblock.com>
2024-09-07 20:00:01 +0200

    Offer details

--- packages/nextjs/db/jobActions.ts
+++ packages/nextjs/db/jobActions.ts
@@ -2,7 +2,7 @@
 
 import { employee, job, NewJob } from "~~/db/schema";
 import { db } from "~~/db/drizzle";
-import { eq } from 'drizzle-orm'
+import { eq, and } from 'drizzle-orm'
 
 
 export const addJobOffer = (newJob: NewJob) => {
@@ -11,4 +11,8 @@ export const addJobOffer = (newJob: NewJob) => {
 
 export const getJobOffers = () => {
   return db.select().from(job).leftJoin(employee, eq(job.employer, employee.wallet))
+}
+
+export const getJob = (employer: string, index: number) => {
+  return db.select().from(job).where(and(eq(job.employer, employer), eq(job.arrayIndex, index))).leftJoin(employee, eq(job.employer, employee.wallet))
 }

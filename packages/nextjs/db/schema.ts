c46fd16d6cf8f3bca9e8bbf4eab7d22c3387b2c6
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2024-09-07 12:56:00 +0200

    🥨 Fix db schema (#18)
    Fix db schema

--- packages/nextjs/db/schema.ts
+++ packages/nextjs/db/schema.ts
@@ -1,7 +1,14 @@
 import { pgTable, text } from 'drizzle-orm/pg-core'
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const employer = pgTable('employer', {
   wallet: text('wallet').primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull(),
 })
+
+export const insertEmployerSchema = createInsertSchema(employer)
+export const selectEmployerSchema = createSelectSchema(employer)
+export type Employer = z.infer<typeof selectEmployerSchema>
+export type NewEmployer = z.infer<typeof insertEmployerSchema>

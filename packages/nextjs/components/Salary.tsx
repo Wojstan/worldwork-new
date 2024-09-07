88fb45ac498f69f72456311223345e22ade3662a
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-09-07 21:58:43 +0200

    Improve Salary component

--- packages/nextjs/components/Salary.tsx
+++ packages/nextjs/components/Salary.tsx
@@ -8,15 +8,16 @@ interface Props {
 export function Salary({ primary, secondary }: Props) {
   return (
     <div className="flex flex-col items-end w-44">
-      <span>
-        <span className="font-bold">${primary}.00</span> USD
-      </span>
-      {secondary && (
-        <span className="flex items-center gap-1">
-          <Image alt="usdc" src="/usdc.png" width={20} height={20} /> <span className="font-bold">{secondary}.00</span>{' '}
-          USDC
+      <span className="flex items-center gap-1">
+          <Image alt="usdc" src="/usdc.png" width={20} height={20}/> <span
+        className="font-bold">{primary}.00</span>{' '}
+        USDC
         </span>
-      )}
+      {secondary && <span className="flex items-center gap-1">
+          <Image alt="wld" src="/wld.png" width={20} height={20}/> <span
+        className="font-bold">{secondary}.00</span>{' '}
+        WLD
+        </span>}
       <span className="text-gray-500 font-light text-sm">Net/month</span>
     </div>
   )

663e34dca769791f78a2853f6940471e8cc783fe
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-09-07 20:40:42 +0200

    Add initial payments

--- packages/nextjs/components/job/CompanyPayments.tsx
+++ packages/nextjs/components/job/CompanyPayments.tsx
@@ -1,6 +1,29 @@
 import { Heading1 } from '../ui/Heading1'
+import { useQuery } from "@tanstack/react-query";
+import { useAccount } from "wagmi";
+import { RequestNetwork, Types } from "@requestnetwork/request-client.js";
 
 export function CompanyPayments() {
+  const { address } = useAccount()
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['payments', address],
+    queryFn: async () => {
+      if (!address) {
+        throw new Error('No wallet provided')
+      }
+      const requestClient = new RequestNetwork({
+        nodeConnectionConfig: {
+          baseURL: "https://gnosis.gateway.request.network",
+        },
+      });
+      const requests = await requestClient.fromIdentity({
+        type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+        value: address,
+      });
+      return requests.map((request) => request.getData());
+    }
+  })
+
   return (
     <div>
       <Heading1>Payments</Heading1>

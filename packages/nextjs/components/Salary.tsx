afbf3a0b4ee268c3d368474c7960606b1612a832
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2024-09-07 22:03:05 +0200

    Fix lint

--- packages/nextjs/components/Salary.tsx
+++ packages/nextjs/components/Salary.tsx
@@ -9,15 +9,14 @@ export function Salary({ primary, secondary }: Props) {
   return (
     <div className="flex flex-col items-end w-44">
       <span className="flex items-center gap-1">
-          <Image alt="usdc" src="/usdc.png" width={20} height={20}/> <span
-        className="font-bold">{primary}.00</span>{' '}
-        USDC
+        <Image alt="usdc" src="/usdc.png" width={20} height={20} /> <span className="font-bold">{primary}.00</span> USDC
+      </span>
+      {secondary && (
+        <span className="flex items-center gap-1">
+          <Image alt="wld" src="/wld.png" width={20} height={20} /> <span className="font-bold">{secondary}.00</span>{' '}
+          WLD
         </span>
-      {secondary && <span className="flex items-center gap-1">
-          <Image alt="wld" src="/wld.png" width={20} height={20}/> <span
-        className="font-bold">{secondary}.00</span>{' '}
-        WLD
-        </span>}
+      )}
       <span className="text-gray-500 font-light text-sm">Net/month</span>
     </div>
   )
